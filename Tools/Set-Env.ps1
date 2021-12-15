96a67d3c77e09d96efb1a78c567b54d653e68b9a
fkthat <fkthat@yandex.ru>
2021-12-15 10:29:13 +0300

    Update Set-Env.ps1 to support an empty user path

--- Tools/Set-Env.ps1
+++ Tools/Set-Env.ps1
@@ -1,12 +1,19 @@
-$MachinePath = 
+$MachinePath =  `
     '%SystemRoot%\System32;' + `
     '%SystemRoot%;' + `
     '%SystemRoot%\System32\Wbem'
 
-$UserPath = `
-    '%ProgramFiles%\dotnet;' + `
-    '%ProgramFiles%\Git\cmd;'
+$UserPath = ''
+    # '%ProgramFiles%\dotnet;' + `
+    # '%ProgramFiles%\Git\cmd;'
 
 setx /M PATH $MachinePath
-setx PATH $UserPath
+
+if($UserPath) {
+    setx PATH $UserPath
+}
+else {
+    Remove-ItemProperty 'HKCU:\Environment' -Name 'PATH'
+}
+
 setx VBOX_USER_HOME "$env:USERPROFILE\VirtualBox\Config"
