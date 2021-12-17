57719cbdc39297560b077f19bb152080477084dc
fkthat <fkthat@yandex.ru>
2021-12-17 19:26:00 +0300

    Update web search module Search.psm1

--- Modules/Search.psm1
+++ Modules/Search.psm1
@@ -1,121 +1,57 @@
-function Open-Search {
+# Search WEB
+
+function Get-SearchEngine {
     [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [string]
+        $Keyword
+    )
+
+    $webData = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Web Data"
+    $tempWebData = New-TemporaryFile
+    Copy-Item $webData $tempWebData
+    try {
+        $q = 'select keyword, url from keywords where is_active = 1'
+        Invoke-SqliteQuery $q -Database $tempWebData |
+            Where-Object { -not $Keyword -or $_ -eq $Keyword }
+    }
+    finally {
+        Remove-Item $tempWebData
+    }
+}
+
+class SearchEngineKeyword : System.Management.Automation.IValidateSetValuesGenerator {
+     [String[]] GetValidValues() {
+        return (Get-SearchEngine | Select-Object -ExpandProperty keyword)
+     }
+ }
+
+function Search-Web {
+	[CmdletBinding()]
     param (
         [Parameter(Mandatory = $true, Position = 0)]
-        [string[]] $Terms,
-        [Parameter(Mandatory = $true)]
-        [string] $UrlFormat
+        [ValidateSet([SearchEngineKeyword])]
+        [string]
+        $Engine,
+        [Parameter(Mandatory = $true, ValueFromRemainingArguments = $true)]
+        [string[]]
+        $Terms
     )
 
-    $Query = $Terms | Join-String -Separator ' '
-    $Query = [System.Uri]::EscapeDataString($Query)
-    $Url = [System.String]::Format($UrlFormat, $Query)
-    Start-Process $Url
+    $url = Get-SearchEngine |
+        Where-Object keyword -eq $Engine |
+        Select-Object -ExpandProperty url
+
+    $t = ($Terms | Join-String -Separator ' ')
+
+    $url = $url -replace '/\{searchTerms\}',('/' + [System.Uri]::EscapeUriString($t)) `
+        -replace '=\{searchTerms\}',('=' + [System.Uri]::EscapeDataString($t))
+
+    Start-Process $url
 }
 
-function Search-Bing {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, ValueFromRemainingArguments)]
-        [string[]] $Terms
-    )
+New-Alias srweb Search-Web -Force
 
-    Open-Search $Terms -UrlFormat `
-        "https://www.bing.com/search?q={0}"
-}
-
-function Search-Api {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, ValueFromRemainingArguments)]
-        [string[]] $Terms
-    )
-
-    Open-Search $Terms -UrlFormat `
-        "https://docs.microsoft.com/en-us/dotnet/api/?term={0}"
-}
-
-function Search-Docs {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, ValueFromRemainingArguments)]
-        [string[]] $Terms
-    )
-
-    Open-Search $Terms -UrlFormat `
-        "https://docs.microsoft.com/en-us/search/?terms={0}"
-}
-
-function Search-Mdn {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, ValueFromRemainingArguments)]
-        [string[]] $Terms
-    )
-
-    Open-Search $Terms -UrlFormat `
-        "https://developer.mozilla.org/en-US/search?q={0}"
-}
-
-function Search-WikiRu {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, ValueFromRemainingArguments)]
-        [string[]] $Terms
-    )
-
-    Open-Search $Terms -UrlFormat `
-        "https://ru.wikipedia.org/w/index.php?search={0}"
-}
-
-function Search-WikiEn {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, ValueFromRemainingArguments)]
-        [string[]] $Terms
-    )
-
-    Open-Search $Terms -UrlFormat `
-        "https://en.wikipedia.org/w/index.php?search={0}"
-}
-
-function Search-Cald {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, ValueFromRemainingArguments)]
-        [string[]] $Terms
-    )
-
-    Open-Search $Terms -UrlFormat `
-        "https://dictionary.cambridge.org/dictionary/english/{0}"
-}
-
-function Search-WikiVim {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, ValueFromRemainingArguments)]
-        [string[]] $Terms
-    )
-
-    Open-Search $Terms -UrlFormat `
-        "https://vim.fandom.com/wiki/Special:Search?search=%s"
-}
-
-
-New-Alias srbing Search-Bing -Force
-New-Alias srapi Search-Api -Force
-New-Alias srdocs Search-docs -Force
-New-Alias srmdn Search-Mdn -Force
-New-Alias srwikiru Search-WikiRu -Force
-New-Alias srwikien Search-WikiEn -Force
-New-Alias srcald Search-Cald -Force
-New-Alias srwikivim Search-WikiVim -Force
-
-Export-ModuleMember 'Search-Bing', 'Search-Api', 'Search-Docs', 
-    'Search-Mdn', 'Search-WikiRu', 'Search-WikiEn', 'Search-Cald',
-    'Search-WikiVim'
-
-Export-ModuleMember -Alias 'srbing', 'srapi', 'srdocs',
-    'srmdn', 'srwikiru', 'srwikien', 'srcald',
-    'srwikivim'
-
+Export-ModuleMember -Function Search-Web, Get-SearchEngine
+Export-ModuleMember -Alias srweb
