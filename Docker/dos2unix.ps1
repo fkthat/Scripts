118a5ee0a6eb3fae34ab08fb9a1249bb22f9c28d
fkthat <fkthat@yandex.ru>
2022-04-30 21:53:55 +0300

    Fix CRLF problem

--- Docker/dos2unix.ps1
+++ Docker/dos2unix.ps1
@@ -1,5 +1,9 @@
+#
+# Run it before invoking docker-compose on the Windows
+#
+
 Get-ChildItem $PSScriptRoot -Recurse -File -Exclude .gitignore,*.ps1 |
     ForEach-Object {
-        $text = (Get-Content $_ -Raw) -replace "`r`n","`n"
+        $text = (Get-Content $_ -Raw) -replace "`r`n","`n" -replace "\s*$",""
         $text > $_
     }
