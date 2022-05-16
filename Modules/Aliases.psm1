cece248d6fbfa6c560b625d495a374838042b623
fkthat <fkthat@yandex.ru>
2022-05-16 14:27:04 +0300

    Update aliases

--- Modules/Aliases.psm1
+++ Modules/Aliases.psm1
@@ -3,37 +3,39 @@
 #
 
 if($PSVersionTable.Platform -eq "Win32NT") {
-    New-Alias scp "${env:SystemRoot}\System32\OpenSSH\scp.exe" -Force
-    New-Alias ssh "${env:SystemRoot}\System32\OpenSSH\ssh.exe" -Force
+    Set-Alias scp "${env:SystemRoot}\System32\OpenSSH\scp.exe"
+    Set-Alias ssh "${env:SystemRoot}\System32\OpenSSH\ssh.exe"
 
-    New-Alias 7z "${env:ProgramFiles}\7-zip\7z.exe" -Force
-    New-Alias bash "${env:ProgramFiles}\Git\bin\bash.exe" -Force
-    New-Alias docker "${env:ProgramFiles}\Docker\Docker\resources\bin\docker.exe" -Force
-    New-Alias less "${env:ProgramFiles}\Git\usr\bin\less.exe" -Force
-    New-Alias minikube "${env:ProgramFiles}\Kubernetes\Minikube\minikube.exe" -Force
-    New-Alias paint "${env:ProgramFiles}\paint.net\PaintDotNet.exe" -Force
-    New-Alias sqlcmd "${env:ProgramFiles}\Microsoft SQL Server\Client SDK\ODBC\170\Tools\Binn\SQLCMD.EXE" -Force
+    Set-Alias 7z "${env:ProgramFiles}\7-zip\7z.exe"
+    Set-Alias bash "${env:ProgramFiles}\Git\bin\bash.exe"
+    Set-Alias docker "${env:ProgramFiles}\Docker\Docker\resources\bin\docker.exe"
+    Set-Alias less "${env:ProgramFiles}\Git\usr\bin\less.exe"
+    Set-Alias minikube "${env:ProgramFiles}\Kubernetes\Minikube\minikube.exe"
+    Set-Alias paint "${env:ProgramFiles}\paint.net\PaintDotNet.exe"
+    Set-Alias sqlcmd "${env:ProgramFiles}\Microsoft SQL Server\Client SDK\ODBC\170\Tools\Binn\SQLCMD.EXE"
 
-    New-Alias az "${env:ProgramFiles(x86)}\Microsoft SDKs\Azure\CLI2\wbin\az.cmd" -Force
-    New-Alias decompile "${env:ProgramFiles(x86)}\Progress\JustDecompile\Libraries\JustDecompile.exe" -Force
-    New-Alias gh "${env:ProgramFiles}\GitHub CLI\gh.exe" -Force
-    New-Alias glab "${env:ProgramFiles(x86)}\glab\glab.exe" -Force
+    Set-Alias az "${env:ProgramFiles(x86)}\Microsoft SDKs\Azure\CLI2\wbin\az.cmd"
+    Set-Alias decompile "${env:ProgramFiles(x86)}\Progress\JustDecompile\Libraries\JustDecompile.exe"
+    Set-Alias gh "${env:ProgramFiles}\GitHub CLI\gh.exe"
+    Set-Alias glab "${env:ProgramFiles(x86)}\glab\glab.exe"
+    Set-Alias dotnet-core-uninstall "${env:ProgramFiles(x86)}\dotnet-core-uninstall\dotnet-core-uninstall.exe"
 
-    New-Alias azds "${env:LocalAppData}\Programs\Azure Data Studio\azuredatastudio.exe" -Force
-    New-Alias dos2unix "${env:LocalAppData}\Programs\dos2unix\bin\dos2unix.exe" -Force
-    New-Alias dtui "${env:LocalAppData}\Programs\dt1.8.3\drop\dtui.exe" -Force
-    New-Alias ffmpeg "${env:LocalAppData}\Programs\ffmpeg\bin\ffmpeg.exe" -Force
-    New-Alias nuget "${env:LocalAppData}\Programs\NuGet\nuget.exe" -Force
-    New-Alias procexp "${env:LocalAppData}\Programs\Sysinternals\ProcessXP\procexp64.exe" -Force
-    New-Alias sqlite3 "${env:LocalAppData}\Programs\Sqlite\sqlite3.exe" -Force
-    New-Alias unix2dos "${env:LocalAppData}\Programs\dos2unix\bin\unix2dos.exe" -Force
-    New-Alias vi "${env:LocalAppData}\Programs\vim\vim82\vim.exe" -Force
-    New-Alias vim "${env:LocalAppData}\Programs\vim\vim82\vim.exe" -Force
-    New-Alias winget "${env:LocalAppData}\Microsoft\WindowsApps\winget.exe" -Force
+    Set-Alias azds "${env:LocalAppData}\Programs\Azure Data Studio\azuredatastudio.exe"
+    Set-Alias dos2unix "${env:LocalAppData}\Programs\dos2unix\bin\dos2unix.exe"
+    Set-Alias dtui "${env:LocalAppData}\Programs\dt1.8.3\drop\dtui.exe"
+    Set-Alias ffmpeg "${env:LocalAppData}\Programs\ffmpeg\bin\ffmpeg.exe"
+    Set-Alias nuget "${env:LocalAppData}\Programs\NuGet\nuget.exe"
+    Set-Alias procexp "${env:LocalAppData}\Programs\Sysinternals\ProcessXP\procexp64.exe"
+    Set-Alias sqlite3 "${env:LocalAppData}\Programs\Sqlite\sqlite3.exe"
+    Set-Alias unix2dos "${env:LocalAppData}\Programs\dos2unix\bin\unix2dos.exe"
+    Set-Alias vi "${env:LocalAppData}\Programs\vim\vim82\vim.exe"
+    Set-Alias vim "${env:LocalAppData}\Programs\vim\vim82\vim.exe"
+    Set-Alias pactester "${env:LocalAppData}\Programs\pactester\pactester.exe"
+    Set-Alias winget "${env:LocalAppData}\Microsoft\WindowsApps\winget.exe"
 }
 
 #
 # PWSH
 #
 
-New-Alias help Get-Help -Force
+Set-Alias help Get-Help
