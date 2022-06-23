6ed468eae7e7e9653baf1520d01e16090598064a
fkthat <fkthat@yandex.ru>
2022-06-23 08:37:11 +0300

    #43: Add module reload to update.ps1

--- update.ps1
+++ update.ps1
@@ -1,4 +1,11 @@
 
 Push-Location $PSScriptRoot
+
 git checkout develop && git pull
+
+# reload modules
+Get-ChildItem -Filter *.psm1 |
+    Select-Object -ExpandProperty FullName |
+    Import-Module -Force
+
 Pop-Location
